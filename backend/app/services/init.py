810f7c06c65ba8ce36940b1f0f469d885a5b1fe1
mathewmoya6-bit <mathewmoya6@gmail.com>
2026-08-30 12:39:51 +0300

    Create init.py

---
+++ backend/app/services/init.py
@@ -0,0 +1,10 @@
+"""
+Services Package
+"""
+from app.services import agent_service, member_service, payment_service
+
+__all__ = [
+    "agent_service",
+    "member_service",
+    "payment_service",
+]
