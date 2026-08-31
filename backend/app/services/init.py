1d76c22f75c0ec2f843ed7c5228c79656f4f661c
mathewmoya6-bit <mathewmoya6@gmail.com>
2026-08-31 13:55:59 +0300

    Update init.py

--- backend/app/services/init.py
+++ backend/app/services/init.py
@@ -1,10 +1,10 @@
 """
 Services Package
 """
-from app.services import agent_service, member_service, payment_service
-
+from app.services import agent_service, member_service, payment_service, membership_service
 __all__ = [
     "agent_service",
     "member_service",
     "payment_service",
+    "membership_service",
 ]
