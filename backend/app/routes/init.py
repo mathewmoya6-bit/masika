2a5e1f81400ff0101fcbcd537c95948cbd1e2375
mathewmoya6-bit <mathewmoya6@gmail.com>
2026-08-31 13:45:15 +0300

    Update init.py

--- backend/app/routes/init.py
+++ backend/app/routes/init.py
@@ -1,12 +1,12 @@
 """
 Routes Package
 """
-from app.routes import public, staff, admin, webhooks, payment
-
+from app.routes import public, staff, admin, webhooks, payment, membership
 __all__ = [
     "public",
     "staff",
     "admin",
     "webhooks",
     "payment",
+    "membership",
 ]
